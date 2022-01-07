d2cdd36fe48e1f7119d8566b34c2fbba0d4c1f13
HenkPieterse <65182816+HenkPieterse@users.noreply.github.com>
2022-01-07 15:09:08 +0200

    Update Pc repair project.py

--- Pc repair project.py
+++ Pc repair project.py
@@ -10,23 +10,28 @@ from PySimpleGUI.PySimpleGUI import CalendarButton
 #y = sg.InputText()
 
 sg.theme('DarkAmber')   # Add a touch of color
+#x = sg.popup_get_date(title=('Choose Date'))
 # All the stuff inside your window.
-layout = [  [sg.Image('workshop logo.png')],
-            [sg.CalendarButton(button_text=('Choose Date'), format=('%d/%m/%Y') , target=('-CAL-')) , 
-                sg.InputText( key=('-CAL-') )],
-            [sg.Image('pngwing.com1.png' ) , sg.Text('Contact Detail:'), sg.InputText() ],
+layout = [  [sg.Image('images/workshop logo.png')],
+            [sg.In(key='-CAL-', visible=True, readonly=True) , sg.CalendarButton(button_text=('Choose Date') , format=('%d/%m/%Y') , target=('-CAL-')) , 
+                ],#sg.InputText( key=('-CAL-') ) ],
+            [sg.Text('Job number:'), sg.InputText() ],
+            [sg.Image('images/pngwing.com1.png' ) , sg.Text('Contact Detail:'), sg.InputText() ],
             [sg.Text('               Name of contact person') , sg.InputText() ],
-            [sg.Text('Enter something on Row 2'), sg.InputText() ],
+            [sg.Text('Repair detail:'), sg.InputText() ],
+            [sg.Text('Action taken on repair:') , sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Pc Reapair forms', layout)
+window = sg.Window('Pc Reapair forms' , layout , icon=('lh_logo_new1.png') )
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, InputL = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered', InputL[2] , 'and' , InputL[3] , InputL[4] )#Images counts to number of inputs. this is only the Text inputs
-    print('The date is:' , )
+    print('You entered', InputL[1] , 'and' , InputL[3] , InputL[4] , InputL[5] , InputL[6] )#Images counts to number of inputs. this is only the Text inputs
+    #print('The date is:' , x )
+
+
 
 window.close()
